01e5c9b5482aea39bf08c2b5e6c570ba7691733e
Jordan Shaw <jrdnshw@gmail.com>
2015-10-24 21:37:44 -0400

    got both wheels go in different directions. first steop in navigation

--- cc-autonomous-vehicle.ino
+++ cc-autonomous-vehicle.ino
@@ -17,8 +17,17 @@ int in4Pin = 3;
 int switchPin = 7;
 int potPin = 0;
 
+// will store last time LED was updated
+unsigned long previousMillis = 0;
+
+// interval at which to blink (milliseconds)
+const long interval = 1000;
+
 void setup()
 {
+
+  Serial.begin(9600);
+  
   //  Motor One
   pinMode(in1Pin, OUTPUT);
   pinMode(in2Pin, OUTPUT);
@@ -36,7 +45,37 @@ void loop()
 {
   int speed = analogRead(potPin) / 4;
   boolean reverse = digitalRead(switchPin);
-  setMotor(speed, reverse);
+//  setMotor(speed, reverse);
+
+  unsigned long currentMillis = millis();
+
+//  if(currentMillis - previousMillis >= interval) {
+//  }
+  drive_forward(speed, reverse);
+  delay(1000);
+  motor_stop(speed, reverse);
+  Serial.println("1");
+  
+  drive_backward(speed, reverse);
+  delay(1000);
+  motor_stop(speed, reverse);
+  Serial.println("2");
+  
+  turn_left(speed, reverse);
+  delay(1000);
+  motor_stop(speed, reverse);
+  Serial.println("3");
+  
+  turn_right(speed, reverse);
+  delay(1000);
+  motor_stop(speed, reverse);
+  Serial.println("4"); 
+  
+  motor_stop( speed, reverse);
+  delay(1000);
+  motor_stop(speed, reverse);
+  Serial.println("5");
+  
 }
 
 void setMotor(int speed, boolean reverse)
@@ -50,4 +89,57 @@ void setMotor(int speed, boolean reverse)
   analogWrite(enablePin2, speed);
   digitalWrite(in3Pin, ! reverse);
   digitalWrite(in4Pin, reverse);
+}
+
+
+// Stearing The Motor
+void motor_stop(int speed, boolean reverse){
+  analogWrite(enablePin, speed);
+  digitalWrite(in1Pin, LOW); 
+  digitalWrite(in2Pin, LOW); 
+
+  analogWrite(enablePin2, speed);
+  digitalWrite(in3Pin, LOW); 
+  digitalWrite(in4Pin, LOW);
+  delay(25);
+}
+
+void drive_forward(int speed, boolean reverse){
+  analogWrite(enablePin, speed);
+  digitalWrite(in1Pin, HIGH); 
+  digitalWrite(in2Pin, LOW); 
+
+  analogWrite(enablePin2, speed);
+  digitalWrite(in3Pin, HIGH); 
+  digitalWrite(in4Pin, LOW); 
+}
+
+void drive_backward(int speed, boolean reverse){
+  analogWrite(enablePin, speed);
+  digitalWrite(in1Pin, LOW); 
+  digitalWrite(in2Pin, HIGH); 
+
+  analogWrite(enablePin2, speed);
+  digitalWrite(in3Pin, LOW); 
+  digitalWrite(in4Pin, HIGH); 
+}
+
+void turn_left(int speed, boolean reverse){
+  analogWrite(enablePin, speed);
+  digitalWrite(in1Pin, LOW); 
+  digitalWrite(in2Pin, HIGH); 
+
+  analogWrite(enablePin2, speed);
+  digitalWrite(in3Pin, HIGH); 
+  digitalWrite(in4Pin, LOW);
+}
+
+void turn_right(int speed, boolean reverse){
+  analogWrite(enablePin, speed);
+  digitalWrite(in1Pin, HIGH); 
+  digitalWrite(in2Pin, LOW); 
+
+  analogWrite(enablePin2, speed);
+  digitalWrite(in3Pin, LOW); 
+  digitalWrite(in4Pin, HIGH); 
 }
