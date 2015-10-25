57894100327eede64a1c5cb04677ee384a131acb
Jordan Shaw <jrdnshw@gmail.com>
2015-10-24 20:20:27 -0400

    added support for two motors identically controlled

--- cc-autonomous-vehicle.ino
+++ cc-autonomous-vehicle.ino
@@ -1,18 +1,34 @@
 /*
 Adafruit Arduino - Lesson 15. Bi-directional Motor
+Jordan Shaw
+Added Secondary Motor
 */
 
+// Motor One
 int enablePin = 11;
 int in1Pin = 10;
 int in2Pin = 9;
+
+// Motor Two
+int enablePin2 = 6;
+int in3Pin = 2;
+int in4Pin = 3;
+
 int switchPin = 7;
 int potPin = 0;
 
 void setup()
 {
+  //  Motor One
   pinMode(in1Pin, OUTPUT);
   pinMode(in2Pin, OUTPUT);
   pinMode(enablePin, OUTPUT);
+
+  //Motor Two
+  pinMode(in3Pin, OUTPUT);
+  pinMode(in4Pin, OUTPUT);
+  pinMode(enablePin2, OUTPUT);
+  
   pinMode(switchPin, INPUT_PULLUP);
 }
 
@@ -25,7 +41,13 @@ void loop()
 
 void setMotor(int speed, boolean reverse)
 {
+//  Motor One
   analogWrite(enablePin, speed);
   digitalWrite(in1Pin, ! reverse);
   digitalWrite(in2Pin, reverse);
+
+//  Motor Two
+  analogWrite(enablePin2, speed);
+  digitalWrite(in3Pin, ! reverse);
+  digitalWrite(in4Pin, reverse);
 }
